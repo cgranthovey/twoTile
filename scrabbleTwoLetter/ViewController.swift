ca8a3c74c3414567040472677e04baf5f8ddedb4
Chris Hovey <chrishovey@chriss-macbook-air.local>
2016-04-11 12:12:40 -0600

    Initial Commit

---
+++ scrabbleTwoLetter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  scrabbleTwoLetter
+//
+//  Created by Chris Hovey on 4/11/16.
+//  Copyright © 2016 Chris Hovey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
