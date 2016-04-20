7c288b8f9dd92153c0129bb313842ec10cd425f2
Chris Hovey <chrishovey@chriss-macbook-air.local>
2016-04-20 11:58:18 -0600

    "choose from multiple words" game is looking good!

--- scrabbleTwoLetter/ChooseCorrectWordGameVC.swift
+++ scrabbleTwoLetter/ChooseCorrectWordGameVC.swift
@@ -14,39 +14,49 @@ class ChooseCorrectWordGameVC: UIViewController {
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
     @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var whichIsAWordLbl: UILabel!
+    @IBOutlet weak var gameEnd: UIStackView!
+    
     
     var memorizingWords = [ScrabbleWord]()
-    var fakeWords = ["Ac", "Af", "Ak", "Ap", "Av", "Br", "Ci", "Da", "Di", "Eg", "Ek", "Ew", "Fi", "Fo", "Ga", "Ge", "Gi", "Gu", "Hu", "Ia", "Ib", "Ie", "Ig", "Ik", "Il", "Im", "Io", "Ip", "Ja", "Je", "Ji", "Ju", "Ke", "Ko", "Ku", "Le", "Lu", "Mr", "Ni", "Ny", "Oa", "Ob", "Og", "Ok", "Ot", "Oz", "Po", "Pu", "Py", "Qa", "Qo", "Ra", "Ri", "Ro", "Ru", "Ry", "Sa", "Se", "Sm", "Su", "Te", "Ts", "Tu", "Ub", "Ud", "Uf", "Ug", "Uk", "Ur", "Va", "Ve", "Vi", "Vo", "Vu", "Wa", "Wi", "Wy", "Wu", "Xa", "Xy", "Xo", "Yi", "Yu", "Ym", "Zo", "Zi"]
+    var fakeWords = ["Ac", "Af", "Ak", "Ap", "Av", "Br", "Ci", "Da", "Di", "Eg", "Ek", "Ew", "Fi", "Fo", "Ga", "Ge", "Gi", "Gu", "Hu", "Ia", "Ib", "Ie", "Ig", "Ik", "Il", "Im", "Io", "Ip", "Iz", "Ja", "Je", "Ji", "Ju", "Ke", "Ko", "Ku", "Le", "Lu", "Mr", "Ni", "Ny", "Oa", "Ob", "Og", "Ok", "Ot", "Oz", "Po", "Pu", "Py", "Qa", "Qo", "Ra", "Ri", "Ro", "Ru", "Ry", "Sa", "Se", "Sm", "Su", "Te", "Ts", "Tu", "Ub", "Ud", "Uf", "Ug", "Uk", "Ur", "Va", "Ve", "Vi", "Vo", "Vu", "Wa", "Wi", "Wy", "Wu", "Xa", "Xy", "Xo", "Yi", "Yu", "Ym", "Zo", "Zi"]
     
     var x: Int = 0
+    var shortArray: [UIButton]!
+    var memorizingWordsCount: Int!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         memorizingWords = DataService.instance.savedWords
         memorizingWords.shuffleInPlace()
         fakeWords.shuffleInPlace()
+        memorizingWordsCount = memorizingWords.count
+        
         resetGame()
-
-
     }
     
-    override func viewDidAppear(animated: Bool) {
-
-    }
-    
-    var shortArray: [UIButton]!
-
     func resetGame(){
-
-        var CurrentWord = memorizingWords[x]
+        
+        if x == memorizingWordsCount{
+            x = 0
+            memorizingWords.shuffleInPlace()
+            fakeWords.shuffleInPlace()
+            resetGame()
+            button1.hidden = true
+            button2.hidden = true
+            button3.hidden = true
+            button4.hidden = true
+            whichIsAWordLbl.hidden = true
+            gameEnd.hidden = false
+            resetButtonColors()
+            return
+        }
+   
+        let CurrentWord = memorizingWords[x]
         
         shortArray = [button1, button2, button3, button4]
         shortArray.shuffleInPlace()
-        
-        button1.backgroundColor = UIColor(red: 89.0/255.0, green: 133.0/255.0, blue: 203.0/255.0, alpha: 1.0)
-        button2.backgroundColor = UIColor(red: 89.0/255.0, green: 133.0/255.0, blue: 203.0/255.0, alpha: 1.0)
-        button3.backgroundColor = UIColor(red: 89.0/255.0, green: 133.0/255.0, blue: 203.0/255.0, alpha: 1.0)
-        button4.backgroundColor = UIColor(red: 89.0/255.0, green: 133.0/255.0, blue: 203.0/255.0, alpha: 1.0)
+        resetButtonColors()
         
         shortArray[0].setTitle(CurrentWord.word, forState: .Normal)
         shortArray[1].setTitle(fakeWords[0], forState: .Normal)
@@ -54,6 +64,18 @@ class ChooseCorrectWordGameVC: UIViewController {
         shortArray[3].setTitle(fakeWords[2], forState: .Normal)
         fakeWords.shuffleInPlace()
         x = x + 1
+        
+        
+
+        
+        
+    }
+    
+    func resetButtonColors(){
+        button1.backgroundColor = UIColor(red: 89.0/255.0, green: 133.0/255.0, blue: 203.0/255.0, alpha: 1.0)
+        button2.backgroundColor = UIColor(red: 89.0/255.0, green: 133.0/255.0, blue: 203.0/255.0, alpha: 1.0)
+        button3.backgroundColor = UIColor(red: 89.0/255.0, green: 133.0/255.0, blue: 203.0/255.0, alpha: 1.0)
+        button4.backgroundColor = UIColor(red: 89.0/255.0, green: 133.0/255.0, blue: 203.0/255.0, alpha: 1.0)
     }
     
     
@@ -112,5 +134,60 @@ class ChooseCorrectWordGameVC: UIViewController {
 //                    }, completion: nil)
 //        })
     }
+    
+    
+    
+    @IBAction func playAgain(sender: AnyObject){
+        
+        self.gameEnd.hidden = true
+        self.button1.hidden = false
+        self.button2.hidden = false
+        self.button3.hidden = false
+        self.button4.hidden = false
+        self.whichIsAWordLbl.hidden = false
+        
+//
+//        UIView.animateWithDuration(0.3, delay: 0, options: UIViewAnimationOptions.TransitionNone, animations: { () -> Void in
+//            self.gameEnd.alpha = 0
+//            }) { (finished: Bool) -> Void in
+//                UIView.animateWithDuration(0.3, delay: 0, options: UIViewAnimationOptions.TransitionNone, animations: { () -> Void in
+//                    self.button1.hidden = false
+//                    self.button2.hidden = false
+//                    self.button3.hidden = false
+//                    self.button4.hidden = false
+//                    self.whichIsAWordLbl.hidden = false
+//                    
+//                    }, completion: nil)
+//        }
+    }
+    
+    @IBAction func gameScreen(sender: AnyObject){
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
